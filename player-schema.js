e158df0c2d93fcfb4c8a81440ff90d2381e0281e
Dave Brudner <dbrudne1@gmail.com>
2019-03-18 22:07:25 -0500

    player lookup

--- player-schema.js
+++ player-schema.js
@@ -1,12 +1,8 @@
 const { graphql, buildSchema } = require("graphql");
 
 const playerSchema = buildSchema(`
-	type Player {
-		points: Float
-	}
-
 	type Query {
-		player(name: String!): Player
+		points: Float
 	}
 `);
 
--- schema.js
+++ schema.js
@@ -1,35 +1,112 @@
+// playerStats: {
+// 	type: GraphQLObjectType,
+// 	async resolve() {
+// 		// const url =
+// "https://infinite-cove-44078.herokuapp.com/stats?name=Kevin+Durant";
+// const res = await fetch(url);
+// const data = await res.json();
+// console.log(data.League.Standard.Stats.latest.ppg);
+// 		return {
+// 			points: 5.3
+// 		};
+// 	}
+// }
 const {
 	graphql,
+	buildSchema,
 	GraphQLSchema,
 	GraphQLObjectType,
-	GraphQLString,
+	GraphQLString
 } = require("graphql");
-const playerSchema = require("./player-schema");
 const fetch = require("node-fetch");
+
+const playerSchema = buildSchema(`
+	type Query {
+		points: Float
+	}
+`);
+
+const Player = new GraphQLObjectType({
+	name: "Player",
+	fields: () => {
+		return {
+			name: {
+				type: GraphQLString
+			},
+			points: {
+				type: GraphQLString
+			},
+			assists: {
+				type: GraphQLString
+			},
+			rebounds: {
+				type: GraphQLString
+			},
+			steals: {
+				type: GraphQLString
+			},
+			assists: {
+				type: GraphQLString
+			},
+			blocks: {
+				type: GraphQLString
+			},
+			minutes: {
+				type: GraphQLString
+			},
+			fieldGoalPercentage: {
+				type: GraphQLString
+			},
+			freeThrowPercentage: {
+				type: GraphQLString
+			}
+		};
+	}
+});
 
 module.exports = new GraphQLSchema({
 	query: new GraphQLObjectType({
 		name: "RootQueryType",
 		fields: {
-			hello: {
-				type: GraphQLString,
-				resolve() {
-					return "world";
+			player: {
+				type: Player,
+				args: {
+					name: {
+						type: GraphQLString
+					}
 				},
-			},
-			playerStats: {
-				type: playerSchema,
-				async resolve() {
-					const url =
-						"https://infinite-cove-44078.herokuapp.com/stats?name=Kevin+Durant";
+				async resolve(parentValue, args) {
+					const url = `https://infinite-cove-44078.herokuapp.com/stats?name=${
+						args.name
+					}`;
 					const res = await fetch(url);
 					const data = await res.json();
-					console.log(data.League.Standard.Stats.latest.ppg);
+					console.log(data);
+
+					const {
+						apg,
+						ppg,
+						bpg,
+						rpg,
+						fgp,
+						ftp,
+						spg,
+						mpg
+					} = data.League.Standard.Stats.latest;
+					console.log(data.League.Standard.Stats.latest);
 					return {
-						points: 5.3,
+						name: args.name,
+						points: ppg,
+						assists: apg,
+						rebounds: rpg,
+						steals: spg,
+						blocks: bpg,
+						minutes: mpg,
+						fieldGoalPercentage: fgp,
+						freeThrowPercentage: ftp
 					};
-				},
-			},
-		},
-	}),
+				}
+			}
+		}
+	})
 });
